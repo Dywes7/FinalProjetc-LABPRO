77045c1c36e3cba58b92e8f328940467dede46e3
Diogo Wesley <115931759+Dywes7@users.noreply.github.com>
2023-12-02 21:09:05 -0300

    Update main.c
    Atualização main

--- FinalProject/main.c
+++ FinalProject/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "pgm.c"
+#include "suavizar_img.c"
 
 /* 
 
@@ -14,7 +16,7 @@
 3. QUANTIZAR IMAGEM E A VERSAO SUAVIZADA
     Gerar arquivos de texto e realizar quantização em N níveis da imagem original e suavizada
 
-3. GERAR MATRIZ SCM (C:\Users\GREAT\Pictures\scm.jpeg) (C:\Users\GREAT\Pictures\scm_2.png)
+4. GERAR MATRIZ SCM (C:\Users\GREAT\Pictures\scm.jpeg) (C:\Users\GREAT\Pictures\scm_2.png)
     Utilizar dos arquivos dos elementos da matriz e da suavizada para gerar matriz SCM
     e gravar em arquivo de texto como vetor.
 
@@ -24,18 +26,23 @@ int main(int argc, char *argv[]){
 
 	struct pgm img;
 
-	if (argc != 3){
-		printf("Formato: \n\t %s <imagemEntrada.pgm> <imagemSaida.pgm>\n", argv[0]);
+	if (argc != 2){
+		printf("Formato: \n\t %s <imagemEntrada.pgm>\n", argv[0]);
 		exit(1);
 	}
 
 	readPGMImage(&img,argv[1]);
 
-	writePGMImage(&img, argv[2]);
+	// writePGMImage(&img, argv[2]);
 
 	viewPGMImage(&img);
+
+	// printf("TIPO: %d\n", img.tipo);
+	// printf("Colunas: %d, Linhas: %d\n", img.c, img.r);
+	// printf("MaxValue: %d\n", img.mv);
+
+	gerarImgSuavizada(&img, argv[1]);
 
 	return 0;
 
 }
-
