3b65f6381ec30379d674709f56bb3065bf96eb79
Diogo Wesley <115931759+Dywes7@users.noreply.github.com>
2023-12-09 19:25:09 -0300

    Delete FinalProject directory

--- FinalProject/src/menu.c
+++
@@ -1,7 +0,0 @@
-#include "../libs/pgm.h"
-void menuFiltroMedio(){
-    printf("Selecionar Filtro da Media...\n");
-    printf("Digite 3 para janela 3x3.\n");
-    printf("Digite 5 para janela 5x5.\n");
-	printf("Digite 7 para janela 7x7.\n");
-}
